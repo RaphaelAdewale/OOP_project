be630c860ff7b5c0c406a7bbe785691f9cdfad77
RaphaelAdewale <raphealadebomi@gmail.com>
2022-07-15 16:46:38 +0100

    Edited class name

--- Person.cs
+++ Person.cs
@@ -1,6 +1,6 @@
 using System;
 
-namespace OOPTutorialProject
+namespace OOPProject
 {
     class Person
     {
--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 
-namespace OOPTutorialProject
+namespace OOPProject
 {
     class Program
     {
